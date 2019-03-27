92eb5373721544ab3a919fbc96091b8c57527a3f
Caroline Pan <abc1588@yahoo.com>
2019-03-27 16:46:18 -0700

    can add student and submit to table

--- src/components/students_table.js
+++ src/components/students_table.js
@@ -1,44 +1,27 @@
 import React, {Component} from 'react';
-import studentData from '../dummy_data/student_list';
+
 
 // console.log('student data: ', studentData);
 
 class StudentTable extends Component {
 
-    // update component state with the student data - to avoid error, set default state to the object
-    state = {
-        students: []
-    }
-
-    componentDidMount() {
-        this.getStudentData();
-    }
-
-    getStudentData() {
-        // call server here to get data
-        this.setState({
-            students: studentData
-        });
-    }
-
     render(){
 
-        //const students = this.state.students; can be abbreviated as in next stmt:
-        const {students} = this.state;
+        const { col = 's12', list } = this.props;
 
         //const studentElements = students.map((item, index, originalArray)=> {
-        const studentElements = students.map((student)=> {
+        const studentElements = list.map((student)=> {
             return (
                 <tr key={student.id}>
                     <td>{student.name}</td>
                     <td>{student.course}</td>
                     <td>{student.grade}</td>
                 </tr>
-            )
+            );
         });
 
         return (
-            <div className="col s12 m8">
+            <div className={`col ${col}`}>
                 <table>
                 <thead>
                 <tr>
